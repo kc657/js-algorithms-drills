bf3490bdff6ed67845be9f3a971b8054f7e58bde
kc657 <kvn.0218@gmail.com>
2017-07-12 14:47:47 -0700

    finished max-contig-subarray practice

--- max-contig-subarray/starter-code.js
+++ max-contig-subarray/starter-code.js
@@ -1,13 +1,35 @@
-function maxContigSubarray(arr) {
+function maxContigSubarray (arr) {
+  let largestGroup = null
+  let largestArray = []
 
+
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] < 0) {
+      console.log('if array does not show up below, then the array was full of damn negatives')
+      continue
+    } else {
+      for (i = 0; i < arr.length; i++) {
+        let sum = arr[i] + arr[i + 1]
+        if (sum > largestGroup) {
+          largestGroup = sum
+          largestArray.shift()
+          largestArray.shift()
+          largestArray.push(arr[i], arr[i + 1])
+        }
+      }
+      console.log(`the sum of the two largest continious subarray  is `largestGroup);
+      console.log(largestArray)
+    }
+  }
 }
 
-testArr1 = [4,7,-1,-12,7];
-testArr2 = [0,8,0,2,3];
-testArr3 = [-1,5,66,7,-8,-22,4];
-testArr4 = [-2,-3,4,1,-1,-5,4];
 
-console.log(maxContigSubarray(testArr1));
-console.log(maxContigSubarray(testArr2));
-console.log(maxContigSubarray(testArr3));
-console.log(maxContigSubarray(testArr4));
+testArr1 = [1, 2, 1, -4, 8]
+testArr2 = [0, 8, 1, 2, 3]
+testArr3 = [-1, 5, 66, 7, -8, -22, 4]
+testArr4 = [-2, -3, -1, -5, -4]
+
+console.log(maxContigSubarray(testArr1))
+console.log(maxContigSubarray(testArr2))
+console.log(maxContigSubarray(testArr3))
+  console.log(maxContigSubarray(testArr4))
